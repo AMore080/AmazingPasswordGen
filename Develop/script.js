7f78405423bde07dc9544579a203438b80a9590b
Alex <alexmore080@gmail.com>
2022-07-23 18:22:20 -0500

    generator functioning as intended

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,31 @@
 // Assignment code here
-var passwordChars = 'abcdefghijkmnlopqrstuvwxyz1234567890'
+var passwordChars = 'abcdefghijkmnlopqrstuvwxyz1234567890';
+let charArray = [];
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+  let passCount;
+  let password = ''
+  passCount = prompt("How many characters would you like?");
+    if (passCount < 8 || passCount > 128){
+      alert("Try again!")
+      textarea.value = ""
+    }
+  
+    for(let i = 0; i < passCount; i++){
+      password += passwordChars.charAt(Math.floor(passCount * Math.random()))
+    }
+    return password;
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-   function generatePassword(){
-    password = Number(prompt("How many characters would you like?"));
-      for(let i = 0; i < password; i++){
-        passwordText += passwordChars.charAt(Math.floor(passwordChars.length * Math.random()))
-      }
-      return passwordText
-  }
+
 
 
   passwordText.value = password;
