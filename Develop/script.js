104d7e87f4aee70b80f7b6aae467e4ee778d2832
Alex <alexmore080@gmail.com>
2022-07-23 00:09:51 -0500

    functioning but with bug

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,5 @@
 // Assignment code here
 var passwordChars = 'abcdefghijkmnlopqrstuvwxyz1234567890'
-const myArray =  passwordChars.split('');
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -9,12 +8,12 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  function generatePassword(){
-    var passCount = prompt("How many characters would you like?");
-    let passwordText
-      for(let i = 0; i < passCount.length; i++){
-      
+   function generatePassword(){
+    password = Number(prompt("How many characters would you like?"));
+      for(let i = 0; i < password; i++){
+        passwordText += passwordChars.charAt(Math.floor(passwordChars.length * Math.random()))
       }
+      return passwordText
   }
 
 
